8d6abbb8f8a44b0124c2faf2bbd29abf9917e364
Arjun-Suresh <arjun.sureshn2000@gmail.com>
2017-09-29 01:14:44 -0500

    Book and author search

--- library/urls.py
+++ library/urls.py
@@ -9,16 +9,30 @@ urlpatterns = [
     url(r'^user/loginoption/$', views.userloginoption, name='userloginoption'),
     # ex: /library/user/signup/
     url(r'^user/signup/$', views.signup, name='signup'),
-    # ex: /library/user/login/
+    # ex: /library/user/checksignup/
     url(r'^user/signup/checksignup/$', views.checksignup, name='checksignup'),    
     # ex: /library/user/login/
     url(r'^user/login/$', views.userlogin, name='userlogin'),
+    # ex: /library/user/login/auth
+    url(r'^user/login/auth$', views.userauthenticate, name='userauthenticate'),
      # ex: /library/user/home
     url(r'^user/home/$', views.userhome, name='userhome'),
     # ex: /library/user/booksearch
     url(r'^user/booksearch/$', views.userbooksearch, name='booksearch'),
-    # ex: /library/user/login/auth
-    url(r'^user/login/auth$', views.userauthenticate, name='userauthenticate'),
+    # ex: /library/user/booksearch/isbn
+    url(r'^user/booksearch/isbn/$', views.userbooksearchisbn, name='booksearchisbn'),
+    # ex: /library/user/booksearch/title
+    url(r'^user/booksearch/title/$', views.userbooksearchtitle, name='booksearchtitle'),
+    # ex: /library/user/booksearch/isbn/checkisbn
+    url(r'^user/booksearch/isbn/checkisbn/$', views.usercheckisbn, name='checkisbn'),
+    # ex: /library/user/booksearch/title/checktitle
+    url(r'^user/booksearch/title/checktitle/$', views.userchecktitle, name='userchecktitle'),
+    # ex: /library/user/booksearch/proceed
+    url(r'^user/booksearch/proceed/$', views.booksearchproceed, name='booksearchproceed'),
+    # ex: /library/user/authorsearch
+    url(r'^user/authorsearch/$', views.userauthorsearch, name='userauthorsearch'),
+    # ex: /library/user/booksearch/isbn/checkisbn
+    url(r'^user/authorsearch/checkauthor/$', views.usercheckauthor, name='usercheckauthor'),
     # ex: /library/admin/login/
     url(r'^admin/login/$', views.adminlogin, name='adminlogin'),
     # ex: /library/admin/login/auth
--- library/views.py
+++ library/views.py
@@ -100,9 +100,195 @@ def userbooksearch(request,arg='',context={}):
         if request.session['userEmail'] == '#' or request.session['Password'] == '#':
             return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
         else:
+            request.session['visited']='true'
             return render(request, 'library/user/booksearch.html', context)
+    else:
+        return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
 
 
+def userbooksearchisbn(request,arg='',context={}):
+    if 'userEmail' in request.session and 'Password' in request.session:
+        if request.session['userEmail'] == '#' or request.session['Password'] == '#':
+            return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+        else:
+            return render(request, 'library/user/booksearchbyisbn.html', context)
+    else:
+        return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+
+
+
+def usercheckisbn(request,arg=''):
+    isbn=request.POST.get("isbn")
+    if 'userEmail' in request.session and 'Password' in request.session:
+        if isbn == None or request.session['userEmail'] == '#' or request.session['Password'] == '#':
+            raise Http404        
+        else:
+            sql="select a.authorname, b.title, b.link from author a,book b where id = (select authorid from haswritten where isbn='"+isbn+"') and b.isbn = '"+isbn+"'"
+            with connection.cursor() as cursor:
+                cursor.execute(sql)
+                returnedVal = cursor.fetchone()
+            if returnedVal == None:
+                request.session['author']='#'
+                request.session['title']='#'
+                request.session['link']='#'
+                return redirectToPage(request,"ISBN Not found.Please try again", '/library/user/booksearch/isbn/')
+            else:
+                request.session['author']=returnedVal[0]
+                request.session['title']=returnedVal[1]
+                request.session['link']=returnedVal[2]
+                
+                return render(request,"library/user/displaybooksisbn.html", {'book':returnedVal,'isbn':isbn})
+
+    else:
+        return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+
+
+
+def userbooksearchtitle(request,arg='',context={}):
+    if 'userEmail' in request.session and 'Password' in request.session:
+        if request.session['userEmail'] == '#' or request.session['Password'] == '#':
+            return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+        else:
+            return render(request, 'library/user/booksearchbytitle.html', context)
+    else:
+        return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+
+
+
+def userchecktitle(request,arg=''):
+    title=request.POST.get("title")
+    if 'userEmail' in request.session and 'Password' in request.session:
+        if title == None or request.session['userEmail'] == '#' or request.session['Password'] == '#':
+            raise Http404        
+        else:
+            sql="select b.title, a.authorname, b.isbn, b.link from book b, author a, haswritten h where b.title='"+title+"' and b.isbn=h.isbn and h.authorid=a.id"
+            with connection.cursor() as cursor:
+                cursor.execute(sql)
+                returnedVal = cursor.fetchall()
+            if returnedVal == None or len(returnedVal) == 0:
+                request.session['author']='#'
+                request.session['title']='#'
+                request.session['link']='#'
+                return redirectToPage(request,"Title Not found.Please try again", '/library/user/booksearch/title/')
+            else:
+                request.session['author']='blah'
+                request.session['title']='blah'
+                request.session['link']='blah'
+                return render(request,"library/user/displaybookstitle.html", {'book':returnedVal,'title':title})
+
+    else:
+        return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+
+    
+
+def booksearchproceed(request,arg=''):
+    isbn=request.POST.get("isbn")
+    if isbn == None:
+        return redirect('/library/user/booksearch/isbn/checkisbn')
+    if 'userEmail' in request.session and 'Password' in request.session:
+        if isbn == None or request.session['userEmail'] == '#' or request.session['Password'] == '#':
+            raise Http404        
+        else:
+            sql="select a.authorname, b.title, b.link from author a,book b where id = (select authorid from haswritten where isbn='"+isbn+"') and b.isbn = '"+isbn+"'"
+            with connection.cursor() as cursor:
+                cursor.execute(sql)
+                returnedVal = cursor.fetchone()
+            author=returnedVal[0]
+            title=returnedVal[1]
+            link=returnedVal[2]
+            if request.session['visited']=='true':
+                request.session['visited']='false'
+                sql="insert into hasread values('"+request.session['userEmail']+"','"+isbn+"')"
+                try:
+                    with connection.cursor() as cursor:
+                        cursor.execute(sql)
+                    connection.commit()
+                except:
+                    print(isbn+' already present\n')
+                sql="select authorid from haswritten where isbn='"+isbn+"'"
+                with connection.cursor() as cursor:
+                    cursor.execute(sql)
+                    returnedVal = cursor.fetchone()
+                authorid=returnedVal[0]
+                sql="select visits from likes where email='"+request.session['userEmail']+"' and authorid="+str(authorid)
+                print(sql)
+                with connection.cursor() as cursor:
+                        cursor.execute(sql)
+                        visits=cursor.fetchone()
+                if visits == None or len(visits)==0:
+                    sql="insert into likes values('"+request.session['userEmail']+"',"+str(authorid)+",1)"
+                    try:
+                        with connection.cursor() as cursor:
+                            cursor.execute(sql)
+                        connection.commit()
+                    except:
+                        print("Error inserting into likes table\n")
+                else:
+                    sql="update likes set visits='"+str(int(visits[0])+1)+"' where email='"+request.session['userEmail']+"' and authorid="+str(authorid)
+                    with connection.cursor() as cursor:
+                        cursor.execute(sql)
+                    connection.commit()
+            return render(request,"library/user/displaybookdetails.html", {'title':title,'author':author,'isbn':isbn,'link':link})
+
+    else:
+        return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+
+
+def userauthorsearch(request,arg='',context={}):
+    if 'userEmail' in request.session and 'Password' in request.session:
+        if request.session['userEmail'] == '#' or request.session['Password'] == '#':
+            return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+        else:
+            request.session['visited']='true'
+            return render(request, 'library/user/authorsearch.html', context)
+    else:
+        return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+
+
+def usercheckauthor(request,arg=''):
+    author=request.POST.get("author")
+    if 'userEmail' in request.session and 'Password' in request.session:
+        if author == None or request.session['userEmail'] == '#' or request.session['Password'] == '#':
+            raise Http404        
+        else:
+            sql="select b.title, a.authorname, b.isbn, b.link from book b, author a, haswritten h where b.isbn=h.isbn and h.authorid=a.id and a.authorname='"+author+"'"
+            with connection.cursor() as cursor:
+                cursor.execute(sql)
+                returnedVal = cursor.fetchall()
+            if returnedVal == None or len(returnedVal) == 0:
+                request.session['author']='#'
+                request.session['title']='#'
+                request.session['link']='#'
+                return redirectToPage(request,"Author not found.Please try again", '/library/user/authorsearch')
+            else:
+                request.session['author']='blah'
+                request.session['title']='blah'
+                request.session['link']='blah'
+                return render(request,"library/user/displaybookstitle.html", {'book':returnedVal})
+
+    else:
+        return redirectToPage(request,"Please Login to proceed", '/library/user/loginoption')
+    
+    
+def userauthenticate(request,arg=''):
+    userEmail=request.POST.get("User_Email")
+    Password=request.POST.get("User_Password")
+    if userEmail == None or Password == None:
+        raise Http404
+    sql="SELECT userpassword from users where email='"+userEmail+"'"
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        returnedPassword = cursor.fetchone()
+    if returnedPassword != None and returnedPassword[0] == Password:
+        request.session['userEmail']=userEmail
+        request.session['Password']=Password
+        request.session['userType']='normal'
+        return redirect("/library/user/home")
+    else:
+        return redirectToPage(request, "Login failure. Please try again", '/library/user/login')
+
+
+        
 def adminauthenticate(request,arg=''):
     userEmail=request.POST.get("User_Email")
     Password=request.POST.get("User_Password")
