e583efdeace040f37020adecf15551fa041d1ce1
Arjun-Suresh <arjun.sureshn2000@gmail.com>
2017-10-05 22:35:30 -0500

    Repaired error

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include,url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'*', include('library.urls')),
+    url(r'[a-z]*', include('library.urls')),
     url(r'^admin/', admin.site.urls),
 ]
