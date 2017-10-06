2b9f0f3330515044d81cfffbd92e389b0922b116
Arjun-Suresh <arjun.sureshn2000@gmail.com>
2017-10-06 01:34:18 -0500

    Name change

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,10 +16,12 @@ import urllib
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_DIR, "templates"),
