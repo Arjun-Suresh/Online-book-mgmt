d7c053cdcbbeb0ea24c2aedc4129f5bf3cca5715
Arjun-Suresh <arjun.sureshn2000@gmail.com>
2017-10-05 22:58:08 -0500

    Repaired error

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,3 +144,6 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIR, "templates"),
+)
