515999fa7778cbc5768cf6a0158d269c3b06b778
Arjun-Suresh <arjun.sureshn2000@gmail.com>
2017-10-05 23:09:20 -0500

    Repaired error

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
