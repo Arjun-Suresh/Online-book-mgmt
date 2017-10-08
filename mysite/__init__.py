e75a09739bd3c47ba2130490f63c5a744fe31dbb
Arjun-Suresh <arjun.sureshn2000@gmail.com>
2017-10-08 13:42:23 -0500

    Minor changes to ini

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -1,7 +1,5 @@
 import pymysql
 pymysql.install_as_MySQLdb()
 #Format for this global variable is 'mysql://username:password@hostname/databaseName
-#DATABASE_URL_CONSTANT = 'mysql://root:password@127.0.0.1/onlinelibrary'
-#ALLOWED_HOSTS_CONSTANT = '127.0.0.1'
-DATABASE_URL_CONSTANT = 'mysql://bccdb0b58a9802:7ee543ed@us-cdbr-iron-east-05.cleardb.net/heroku_e9cedb276cb7f86'
-ALLOWED_HOSTS_CONSTANT = 'desolate-everglades-70182.herokuapp.com'
+DATABASE_URL_CONSTANT = 'mysql://root:password@127.0.0.1/onlinelibrary'
+ALLOWED_HOSTS_CONSTANT = '127.0.0.1'
